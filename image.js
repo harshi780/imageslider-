629bad243d8456e8e41bc0ca808408a181049534
harshi780 <158986030+harshi780@users.noreply.github.com>
2024-08-17 17:10:34 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,29 @@
+// Initialize Swiper
+var swiper = new Swiper('.tranding-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
+});
+      
