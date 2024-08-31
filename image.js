5cd9cea6e2264f3e90b1ede4814e884b01f9c5b7
harshi780 <158986030+harshi780@users.noreply.github.com>
2024-08-31 20:05:16 +0530

    Update image.js

--- image.js
+++ image.js
@@ -1,13 +1,36 @@
-const slide = document.querySelector('.slide');
-const items = document.querySelectorAll('.item');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-let index = 0;
+const slides = document.querySelector('.slides');
+    const slide = document.querySelectorAll('.slide');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    let index = 0;
+    let autoSlide;
 
-function showSlide(i) {
-    index = (i + items.length) % items.length;
-    slide.style.transform = `translateX(-${index * 100}%)`;
-}
+    function showSlide(i) {
+        index = (i + slide.length) % slide.length;
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
 
-prevButton.addEventListener('click', () => showSlide(index - 1));
-nextButton.addEventListener('click', () => showSlide(index + 1));     
+    function startAutoSlide() {
+        autoSlide = setInterval(() => showSlide(index + 1), 3000);
+    }
+
+    function stopAutoSlide() {
+        clearInterval(autoSlide);
+    }
+
+    prevButton.addEventListener('click', () => {
+        stopAutoSlide();
+        showSlide(index - 1);
+        startAutoSlide();
+    });
+
+    nextButton.addEventListener('click', () => {
+        stopAutoSlide();
+        showSlide(index + 1);
+        startAutoSlide();
+    });
+
+    slides.addEventListener('mouseover', stopAutoSlide);
+    slides.addEventListener('mouseout', startAutoSlide);
+
+    startAutoSlide();
